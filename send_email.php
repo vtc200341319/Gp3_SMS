14bb1d504f30fb768275c54419ce940b3f766947
vtc200341319 <vtc200341319@gmail.com>
2023-07-13 21:25:19 +0800

    GP3

--- send_email.php
+++
@@ -1,6 +0,0 @@
-<?php
-function send_email($to, $subject, $body) {
-    // This is a placeholder function for sending emails.
-    // Replace this function with your preferred email sending method.
-    echo "Email sent to {$to} with subject '{$subject}' and body '{$body}'";
-}
